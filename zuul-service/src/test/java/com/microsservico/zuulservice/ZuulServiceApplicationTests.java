3e2fb79550881646f3ba858d491d99c141359792
yurilsn <luizyuri57@gmail.com>
2023-12-27 18:33:41 -0300

    first commit

---
+++ zuul-service/src/test/java/com/microsservico/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservico.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
