52a8d1c700955ef3a4604d44b7662673a626ab05
yurilsn <luizyuri57@gmail.com>
2023-12-29 17:48:16 -0300

    Adcionando gerenciamento de animais

---
+++ gerenciamento-de-animais/src/main/java/com/microsservico/gerenciamentodeanimais/GerenciamentoDeAnimaisApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.gerenciamentodeanimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAnimaisApplication.class, args);
+	}
+
+}
